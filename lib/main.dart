75c539ad4890eccca499ac3d793cd81271f3a43a
Musab-Al-omari <musabsomari@gmail.com>
2021-08-30 19:06:01 +0300

    the diffrent between puplic and privet in dart

--- lib/main.dart
+++ lib/main.dart
@@ -4,14 +4,40 @@ void main() {
   runApp(new MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    // TODO: implement createState
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionsIndex = 0;
+  var questions = ['who are you ', 'why you are here'];
+  void _pressed() {
+    setState(() {
+      _questionsIndex++;
+    });
+  }
+
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('my first app'),
         ),
-        body: Text('hello'),
+        body: Column(
+          children: [
+            Text(questions[_questionsIndex]),
+            ElevatedButton(
+                onPressed: () => _pressed(), child: Text('answer 1')),
+            ElevatedButton(
+                onPressed: () => _pressed(), child: Text('answer 2')),
+            ElevatedButton(
+                onPressed: () => _pressed(), child: Text('answer 3')),
+          ],
+        ),
       ),
     );
   }
